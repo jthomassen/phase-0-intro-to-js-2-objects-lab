3b689a906b693fb9c21af520b2554d234f148126
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-09 17:07:19 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    streetAddress: '888 Logan St',
+    name: 'Jeff'
+}
+
+function updateEmployeeWithKeyAndValue(empObj, key, value){
+    let temp = Object.assign({}, empObj);
+    temp[key] = value
+    return temp
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value
+   return employee
+    // return Object.assign(employee, { [key]: value})
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign({}, employee)
+    delete newObject[key]
+    return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
